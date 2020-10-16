081f75390c17d2224d8e3c9b739d9668b52eac3a
jagseer-singh <61871052+jagseer-singh@users.noreply.github.com>
2020-10-16 23:46:06 +0530

    Add files via upload

---
+++ WebTech_Assignment4/Answer2.java
@@ -0,0 +1,10 @@
+public class Answer2 {
+    public static void main(String[] args) {
+        int v1 = -1;
+        byte v2 = (byte) v1;
+        char v3 = (char) v2;
+        int v4 = (int) v3;
+        System.out.println(v4);
+
+    }
+}
